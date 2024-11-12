f7450b13ad86d4667c41c740512ae897762d5349
Elena Haurova <elenita@elenita-mac.lan>
2024-11-12 23:23:13 +0000

    new approach + new date formatting

--- tests/test.ui.spec.ts
+++ tests/test.ui.spec.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@playwright/test';
 import { Login } from '../page-objects/login';
-import { Helpers } from '../page-objects/helpers';
 
 test.beforeEach(async ({ page }) => {
   const login = new Login(page);
@@ -14,30 +13,36 @@ test.beforeEach(async ({ page }) => {
   await login.loginToMicrosoftAcc(process.env.USERNAME!, process.env.PASSWORD!);
   await expect(page).toHaveTitle('velixo-test-assignment.xlsx - Microsoft Excel Online');
 
-  // Clear clipboard so in case copy doesn't work properly and by some chance clipboard has the today date, the test won't pass
+  // clear clipboard so in case copy doesn't work properly and by some chance clipboard has the today date, the test won't pass
   await page.evaluate(() => {
     navigator.clipboard.writeText('');
   });
 });
 
 test('verify TODAY() function', async ({ page }) => {
-  const helper = new Helpers(page);
-  await page.waitForTimeout(3000); // timeout to ensure that the page is loaded
-  await page.mouse.move(550, 490);
-  await page.waitForTimeout(2000); // timeout to let the movemement finish
-  await page.mouse.click(550, 490);
-  await page.keyboard.type('=TODAY()');
-  await page.keyboard.press('Enter');
-  await page.keyboard.press('ArrowUp');
+  const frame = page.locator('#WacFrame_Excel_0').contentFrame();
+  const selectCell = frame.locator('#FormulaBar-NameBox-input')
+  const insertFunctionButton = frame.getByRole('button', {name: "insert function"})
+  const searchForFunctionTextField = frame.getByPlaceholder('Search for a function')
+  const insertButtonDialog = frame.getByRole('button', {name: 'Insert'})
+  const today = new Date()
+  const todayDate = (`${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`)
+  let clipboardContent: string
+  
+  await selectCell.click()
+  await selectCell.fill('A2')
+  await page.keyboard.press('Enter'); 
+  await insertFunctionButton.click()  
+  await searchForFunctionTextField.fill('today')
+  await insertButtonDialog.click()
   await page.keyboard.press('Control+C');
-  await page.waitForTimeout(2000); // timeout to let the clipboard update
+  await page.waitForTimeout(3000) // necessary timeout for clipboard to update
   await page.keyboard.press('Delete');
 
-  const clipboardContent = await page.evaluate(async () => {
+  clipboardContent = await page.evaluate(async () => {
     return await navigator.clipboard.readText();
   });
 
-  console.log('clipboard: ' + clipboardContent, 'today: ' + await helper.dateFormatted(new Date()))
-  expect(clipboardContent).toContain(await helper.dateFormatted(new Date())); 
+  expect(clipboardContent).toContain(todayDate); 
 });
  
